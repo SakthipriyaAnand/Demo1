5e1881a721d633108122415acf1d8cdc10c5b455
SakthipriyaAnand <bsakthipriya12793@gmail.com>
2023-10-06 15:52:15 +0530

    updated

--- src/demo1/Firefox.java
+++ src/demo1/Firefox.java
@@ -11,15 +11,16 @@ public class Firefox {
 		System.setProperty("webdriver.firefox.marionette", "C:\\Users\\al khat al asfar\\Desktop\\selenium chrome\\firefox\\geckodriver.exe");
 		//FirefoxDriver driver = new FirefoxDriver();
 		
-		WebDriver driver = new FirefoxDriver();// we can use both the methods
+		WebDriver driver = new FirefoxDriver();// we can use both the methods webDriver/FirefoxDriver
 		driver.get("https://www.myntra.com/");
 		driver.manage().window().maximize();
 		
-//		WebElement men = driver.findElement(By.className("desktop-main"));
-//		men.click();
-		
-		WebElement women = driver.findElement(By.linkText("/shop/women"));
+		WebElement women = driver.findElement(By.linkText("women"));
 		women.click();
+		WebElement men = driver.findElement(By.linkText("https://assets.myntassets.com/w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/e72c82f2-cfd5-4f9a-b1b0-ba9e2b3e51251598892519506-W.jpg"));
+		men.click();
+		
+		
 
 	}
 
